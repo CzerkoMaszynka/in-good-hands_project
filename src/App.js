778f448d6086c2c4eb3e84e365e38e1f8f6feebd
Damian <damian.czerech@gmail.com>
2019-12-03 17:56:16 +0100

    Creation and configuration of basic App files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <h1 className="FastRed">hi</h1>
+  );
+}
+
+export default App;
