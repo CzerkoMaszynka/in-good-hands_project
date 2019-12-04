f32aaa8dc9b70108f72b115017c2b87125883471
Damian <damian.czerech@gmail.com>
2019-12-04 15:02:28 +0100

    Final App configuration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage';
 
 
 function App() {
   return (
-    <h1 className="FastRed">hi</h1>
+    <Router>
+      <Switch>
+        <Route path="/" component={LandingPage} />
+      </Switch>
+    </Router>
+
   );
 }
 
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from './Header';
+
+const LandingPage = () => (
+  <>
+    <div>LandingPage</div>
+    <Header />
+  </>
+)
+
+export default LandingPage;
