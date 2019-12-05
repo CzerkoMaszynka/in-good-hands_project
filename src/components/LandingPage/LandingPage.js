03c03ac3ce6fad9f9cbd8a7f7b9f6ea411196024
Damian <damian.czerech@gmail.com>
2019-12-05 15:32:26 +0100

    Third section created & styled

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import ThreeColumns from './ThreeColumns';
+import FourSteps from './FourSteps';
 
 const LandingPage = () => (
   <>
     <Header />
     <ThreeColumns />
+    <FourSteps />
   </>
 )
 
