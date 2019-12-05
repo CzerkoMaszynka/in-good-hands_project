ca7a54b2502b536520538f1d1db6c2fd9068854b
Damian <damian.czerech@gmail.com>
2019-12-05 14:12:43 +0100

    section 2 created & styled

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './Header';
+import ThreeColumns from './ThreeColumns';
 
 const LandingPage = () => (
   <>
     <Header />
+    <ThreeColumns />
   </>
 )
 
